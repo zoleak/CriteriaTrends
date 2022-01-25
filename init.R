bcf9d1f4a6ec49fec4b5fa480653ed8facbd0b13
zoleak <36116239+zoleak@users.noreply.github.com>
2022-01-25 18:53:49 -0500

    update init.R

--- init.R
+++ init.R
@@ -2,7 +2,7 @@
 #
 # Example R code to install packages if not already installed
 #
-my_packages = c("shiny", "tidyverse", "readxl","shinycssloaders")
+my_packages = c("shiny", "tidyverse", "readxl","shinycssloaders","shinyauthr")
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
